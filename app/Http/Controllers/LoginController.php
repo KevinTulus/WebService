1b6e929022d23239d76f4377fced8f6af3cf1671
immanuel1210 <immanuelsimarsoit@gmail.com>
2023-05-14 15:14:29 +0700

    Add Login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+
+class LoginController extends Controller
+{
+    public function login(){
+        if(Auth::check()){
+            return redirect ('/');
+        }else{
+            return view('login');
+        }       
+    }
+
+    public function authen(Request $request){
+        // dd($request->all());
+            $credential = $request->validate([
+                'email' => ['required','email'],
+                'password' => ['required']
+            ]);
+            if(Auth::attempt($credential)){
+                $request->session()->regenerate();
+
+                return redirect()->intended('/');
+            }else{
+                return back()->with('error','Email atau Password yang Dimasukkan Salah');
+            }
+    }
+}
